7ed19d0eaba304021c89cb04e67738837eada7d0
karl ducta <karlducta022@gmail.com>
2023-10-21 15:15:58 +0800

    --- CREATING AND REUSING COMPONENTS ---

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ function App() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+function Pizza() {
+  return (
+    <div>
+      <img src="pizzas/spinaci.jpg" alt="pizza spinaci" />
+      <h1>Pizza</h1>
     </div>
   );
 }
