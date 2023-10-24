b4be93d6fb431170bcb0eb73c5f3fba6bd2f4154
karl ducta <karlducta022@gmail.com>
2023-10-24 15:19:43 +0800

    --- React Fragments --- Allow to have more than one element in JSX ---

--- src/index.js
+++ src/index.js
@@ -33,11 +33,19 @@ const Menu = () => {
       <h2>Our Menu</h2>
 
       {numPizza > 0 ? (
-        <ul className="pizzas">
-          {pizzaData.map((pizza) => (
-            <Pizza pizzaObj={pizza} key={pizza.name} />
-          ))}
-        </ul>
+        <>
+          {/* React.Fragment  ->   "<>" or "<React.Fragment key="">" */}
+          <p>
+            Authentic Italian cuisine. 6 creative dishes to choose from. All
+            from our stone oven, all organic, all delicious.
+          </p>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObj={pizza} key={pizza.name} />
+            ))}
+          </ul>
+          {/* React.Fragment  ->   "</>" or "</React.Fragment>" */}
+        </>
       ) : (
         <p>We're still working with our menu. Please come back later.</p>
       )}
