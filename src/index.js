354990568b73cf6b32be054e60408128f494f457
karl ducta <karlducta022@gmail.com>
2023-10-24 14:50:55 +0800

    --- Conditional Rendering with Ternaries ---

--- src/index.js
+++ src/index.js
@@ -25,18 +25,21 @@ const Header = () => {
 
 const Menu = () => {
   const pizzas = pizzaData;
+  // const pizzas = [];
   const numPizza = pizzas.length;
 
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {numPizza > 0 && (
+      {numPizza > 0 ? (
         <ul className="pizzas">
           {pizzaData.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working with our menu. Please come back later.</p>
       )}
     </main>
   );
@@ -63,11 +66,15 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open Until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
